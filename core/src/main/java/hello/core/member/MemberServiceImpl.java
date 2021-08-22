f98ed8a5e6016d029e4e992059176c62df679ded
yulhee741 <dbfgml980413@gmail.com>
2021-08-22 12:43:01 +0900

    AppConfig 리팩토링

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -4,7 +4,9 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
-
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
