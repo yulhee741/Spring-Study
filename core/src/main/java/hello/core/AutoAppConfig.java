086fd28f404729ba6382503f5ee9fa76a64b0782
yulhee741 <dbfgml980413@gmail.com>
2021-08-28 17:53:40 +0900

    롬복 라이브러리 사용

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -15,8 +15,8 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
