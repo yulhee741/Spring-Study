41af1ae6b468eb2c1519b0e7096607b052fe732e
yulhee741 <dbfgml980413@gmail.com>
2021-09-05 19:18:03 +0900

    기타 정보

--- servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -18,8 +18,8 @@ public class RequestHeaderServlet extends HttpServlet {
         printStartLine(request);
         printHeaders(request);
         printHeaderUtils(request);
-        //printEtc(request);
-        //response.getWriter().write("ok");
+        printEtc(request);
+        response.getWriter().write("ok");
     }
 
 
@@ -85,6 +85,27 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.getCharacterEncoding() = " +
                 request.getCharacterEncoding());
         System.out.println("--- Header 편의 조회 end ---");
+        System.out.println();
+    }
+
+    private void printEtc(HttpServletRequest request) {
+        System.out.println("--- 기타 조회 start ---");
+        System.out.println("[Remote 정보]");
+        System.out.println("request.getRemoteHost() = " +
+                request.getRemoteHost()); //
+        System.out.println("request.getRemoteAddr() = " +
+                request.getRemoteAddr()); //
+        System.out.println("request.getRemotePort() = " +
+                request.getRemotePort()); //
+        System.out.println();
+        System.out.println("[Local 정보]");
+        System.out.println("request.getLocalName() = " +
+                request.getLocalName()); //
+        System.out.println("request.getLocalAddr() = " +
+                request.getLocalAddr()); //
+        System.out.println("request.getLocalPort() = " +
+                request.getLocalPort()); //
+        System.out.println("--- 기타 조회 end ---");
         System.out.println();
     }
 }
