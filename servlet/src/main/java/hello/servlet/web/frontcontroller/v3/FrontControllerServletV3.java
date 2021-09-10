c7a2b8d0e97521dabec2d9c2c46fa078a630b2a9
yulhee741 <dbfgml980413@gmail.com>
2021-09-10 17:56:15 +0900

    실용적인 컨트롤러 만들기

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -25,9 +25,9 @@ public class FrontControllerServletV3 extends HttpServlet {
     private Map<String, ControllerV3> controllerMap = new HashMap<>();
 
     public FrontControllerServletV3() {
-        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV3());
-        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV3());
-        controllerMap.put("/front-controller/v2/members", new MemberListControllerV3());
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
     }
 
     @Override
